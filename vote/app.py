6e66a6cdcd6bf8080b79f0e69f6413cf3b2eb4da
David Giffin <david@giffin.org>
2021-03-04 04:02:27 -0500

    biden vs trump

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Bidenie")
-option_b = os.getenv('OPTION_B', "Trumpie")
+option_a = os.getenv('OPTION_A', "Biden")
+option_b = os.getenv('OPTION_B', "Trump")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
